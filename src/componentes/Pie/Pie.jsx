37db9d4d39ad28a89dd69e33cf1a98fa838512c6
JoseMariaGarreH <josemariagarrehermosilla5@gmail.com>
2025-02-14 14:22:43 +0100

    Modificado css del Home para darle otra vista a las imagenes y cambiada salida de un div por un fragment

--- src/componentes/Pie/Pie.jsx
+++ src/componentes/Pie/Pie.jsx
@@ -7,70 +7,72 @@ const Pie = () => {
     const { idioma } = useContext(IdiomaContext);
 
     return (
-        <div className="row">
-            <footer className="fondofooter">
-                <div className="text-center fondosegundofooter">
-                    <div className="row">
-                        <div className="col-12 col-md-3">
-                            <h5 className="font-weight-bold" >{idioma.temas.op1}</h5>
-                            <h2 className="font-weight-bold">0</h2>
-                        </div>
-                        <div className="col-12 col-md-3 mx-auto">
-                            <h5 className="font-weight-bold">{idioma.temas.op2}</h5>
-                            <h2 className="font-weight-bold">0</h2>
-                        </div>
-                        <div className="col-12 col-md-3">
-                            <h5 className="font-weight-bold">{idioma.temas.op3}</h5>
-                            <h2 className="font-weight-bold">0</h2>
+        <>
+            <div className="row">
+                <footer className="fondofooter">
+                    <div className="text-center fondosegundofooter">
+                        <div className="row">
+                            <div className="col-12 col-md-3">
+                                <h5 className="font-weight-bold" >{idioma.temas.op1}</h5>
+                                <h2 className="font-weight-bold">0</h2>
+                            </div>
+                            <div className="col-12 col-md-3 mx-auto">
+                                <h5 className="font-weight-bold">{idioma.temas.op2}</h5>
+                                <h2 className="font-weight-bold">0</h2>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <h5 className="font-weight-bold">{idioma.temas.op3}</h5>
+                                <h2 className="font-weight-bold">0</h2>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="text-center">
-                    <h6><a className="btn" href="https://cifpcarlos3.es/es" target="_blank">CIFP Carlos III</a></h6>
-                    <ul className="list-unstyled list-inline">
-                        <li className="list-inline-item">C/ Carlos III, 30201 - Cartagena | </li>
-                        <li className="list-inline-item">30019702@murciaeduca.es | </li>
-                        <li className="list-inline-item">968321301</li>
-                    </ul>
-                    <ul className="list-unstyled list-inline">
-                        <li className="list-inline-item">
-                            <a href="https://www.facebook.com/cifpcarlos3" target="_blank">
-                                <img className="iconos" src="src/assets/facebook.svg" alt="Facebook" />
-                            </a>
-                        </li>
-                        <li className="list-inline-item">
-                            <a href="https://x.com/cifpcarlos3" target="_blank">
-                                <img className="iconos" src="src/assets/x_light.svg" alt="X" />
-                            </a>
-                        </li>
-                        <li className="list-inline-item">
-                            <a href="https://www.instagram.com/cifpcarlos3" target="_blank">
-                                <img className="iconos" src="src/assets/instagram.svg" alt="Instagram" />
-                            </a>
-                        </li>
-                        <li className="list-inline-item">
-                            <a href="https://www.tiktok.com/@cifpcarlos3" target="_blank">
-                                <img className="iconos" src="src/assets/tiktok.svg" alt="Tiktok" />
-                            </a>
-                        </li>
-                        <li className="list-inline-item">
-                            <a href="https://www.youtube.com/@cifpcarlostercero" target="_blank">
-                                <img className="iconos" src="src/assets/youtube.svg" alt="Youtube" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="text-center">
-                    <ul className="list-unstyled list-inline">
-                        <li className="list-inline-item">
-                            <img src="src/img/mp-logoReves.png" alt="Logo" className="logofooter align-baseline" ></img>
-                        </li>
-                        <li className="list-inline-item">Marca Personal FP</li>
-                        <li className="list-inline-item"> | {idioma.pie.op1} CFGS {idioma.pie.op2} © 2024</li>
-                    </ul>
-                </div>
-            </footer>
-        </div>
+                    <div className="text-center">
+                        <h6><a className="btn" href="https://cifpcarlos3.es/es" target="_blank">CIFP Carlos III</a></h6>
+                        <ul className="list-unstyled list-inline">
+                            <li className="list-inline-item">C/ Carlos III, 30201 - Cartagena | </li>
+                            <li className="list-inline-item">30019702@murciaeduca.es | </li>
+                            <li className="list-inline-item">968321301</li>
+                        </ul>
+                        <ul className="list-unstyled list-inline">
+                            <li className="list-inline-item">
+                                <a href="https://www.facebook.com/cifpcarlos3" target="_blank">
+                                    <img className="iconos" src="src/assets/facebook.svg" alt="Facebook" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://x.com/cifpcarlos3" target="_blank">
+                                    <img className="iconos" src="src/assets/x_light.svg" alt="X" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://www.instagram.com/cifpcarlos3" target="_blank">
+                                    <img className="iconos" src="src/assets/instagram.svg" alt="Instagram" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://www.tiktok.com/@cifpcarlos3" target="_blank">
+                                    <img className="iconos" src="src/assets/tiktok.svg" alt="Tiktok" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://www.youtube.com/@cifpcarlostercero" target="_blank">
+                                    <img className="iconos" src="src/assets/youtube.svg" alt="Youtube" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="text-center">
+                        <ul className="list-unstyled list-inline">
+                            <li className="list-inline-item">
+                                <img src="src/img/mp-logoReves.png" alt="Logo" className="logofooter align-baseline" ></img>
+                            </li>
+                            <li className="list-inline-item">Marca Personal FP</li>
+                            <li className="list-inline-item"> | {idioma.pie.op1} CFGS {idioma.pie.op2} © 2024</li>
+                        </ul>
+                    </div>
+                </footer>
+            </div>
+        </>
     )
 }
 
--- src/paginas/Home/Home.jsx
+++ src/paginas/Home/Home.jsx
@@ -13,7 +13,7 @@ const Menu = () => {
             <div className="row">
                 <div className="col-12 col-sm-6 col-md-4 carta-temas">
                     <Link to='/empresas'>
-                        <img src="src/img/empresa.jpg" className="img-fluid" alt="Empresa"></img>
+                        <img src="src/img/empresa.jpg" className="img-fluid img-temas" alt="Empresa"></img>
                         <div className="text-center texto-elevado">
                             <p>{idioma.temas.op1}</p>
                         </div>
@@ -21,7 +21,7 @@ const Menu = () => {
                 </div>
                 <div className="col-12 col-sm-6 col-md-4 carta-temas">
                     <Link to='/centroeducativo'>
-                        <img src="src/img/school.jpg" className="img-fluid" alt="Centro educativo"></img>
+                        <img src="src/img/school.jpg" className="img-fluid img-temas" alt="Centro educativo"></img>
                         <div className="text-center texto-elevado">
                             <p>{idioma.temas.op2}</p>
                         </div>
@@ -29,7 +29,7 @@ const Menu = () => {
                 </div>
                 <div className="col-12 col-sm-6 col-md-4 carta-temas">
                     <Link to='/alumnos'>
-                        <img src="src/img/student.jpg" className="img-fluid" alt="Alumnos"></img>
+                        <img src="src/img/student.jpg" className="img-fluid img-temas" alt="Alumnos"></img>
                         <div className="text-center texto-elevado">
                             <p>{idioma.temas.op3}</p>
                         </div>
