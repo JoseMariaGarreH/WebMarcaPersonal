476261f46f4c6f2993ecb609a1508a932c7c0ded
JoseMariaGarreH <josemariagarrehermosilla5@gmail.com>
2025-02-18 13:50:58 +0100

    Añadido MenuEmpresa

---
+++ src/componentes/MenuEmpresa.jsx
@@ -0,0 +1,12 @@
+const MenueEmpresa = () => {
+
+    return(
+        <div className="row">
+            <div className="col-12">
+                
+            </div>
+        </div>
+    )
+}
+
+export default MenuEmpresa;
